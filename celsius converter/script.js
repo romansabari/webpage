1893678de33bd996cafa23b757ad5558592eba1c
sabari <sabarinath1312@gmail.com>
2024-07-23 09:55:55 +0530

    Add files via upload

---
+++ celsius converter/script.js
@@ -0,0 +1,16 @@
+ alert("welcome")
+
+function convert(){
+
+var celval=Number(document.getElementById("input").value)
+var fahval=(celval*9/5)+32
+var result=document.getElementById("result").innerHTML=fahval + " Fahrenheit"
+}
+
+
+
+
+
+
+
+//(20°C × 9/5) + 32 = 68°F
